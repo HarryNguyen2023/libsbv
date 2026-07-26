5bffd89d41b116d68bceb7ba2f2fd3779875787d
HarryNguyen2023 <gianguyenlamvinh2002@gmail.com>
2026-07-26 11:44:21 +0700

    SBV13: Clean up sbv_task.c

--- sbv_task.c
+++ sbv_task.c
@@ -17,6 +17,15 @@
 #include "sbv_control_balance.h"
 
 /************************ Gloabal variables declaration ***************************/
+/* Static task objects */
+#define STACK_SIZE_BASE 256
+
+static StackType_t uart_rx_stack[STACK_SIZE_BASE];
+static StackType_t balance_crtl_stack[STACK_SIZE_BASE * 2];
+
+// sbv_rtos_task_handle_t sbv_debug_handle;
+sbv_rtos_task_handle_t sbv_uart_rx_handle;
+sbv_rtos_task_handle_t sbv_balance_ctrl_handle;
 
 /* Control obbjects */
 sbv_control_balance_t   sbv_control_balance;
@@ -30,16 +39,11 @@ extern sbv_uart_dma_handle_t hdma_usart1_rx;
 extern sbv_can_handle_t      hcan;
 sbv_gpio_num_t uart_pin[2] = {SBV_GPIO_NUM_13, SBV_GPIO_NUM_7};
 
-// sbv_rtos_task_handle_t sbv_debug_handle;
-sbv_rtos_task_handle_t sbv_uart_rx_handle;
-sbv_rtos_task_handle_t sbv_balance_ctrl_handle;
+
 
 sbv_uart_instance_t *sbv_uart_1 = NULL;
 
-#define STACK_SIZE 256
 
-static StackType_t uart_rx_stack[STACK_SIZE];
-static StackType_t balance_crtl_stack[STACK_SIZE * 2];
 
 void
 sbv_init(void)
@@ -76,8 +80,8 @@ sbv_task_init(void)
 }
 
 /*
- * Task for testing speed + twist control of the robot
-*/
+ * Task for control of the robot
+ */
 void
 sbv_task_balance_control(void *param)
 {
@@ -106,44 +110,6 @@ sbv_task_balance_control(void *param)
     }
 }
 
-// static void
-// sbv_task_print_rx(uint8_t *data, uint8_t len)
-// {
-//     uint16_t timeout_ms = 100;
-//     sbv_uart_tx_send_data((uint8_t *)"Received: ", 10, timeout_ms);
-//     sbv_uart_tx_send_data(data, len, timeout_ms);
-//     sbv_uart_tx_send_data((uint8_t *)"\r\n", 2, timeout_ms);
-// }
-
-
-// /*
-//  * Task for transmitting debugging information via UART
-//  */
-// void
-// sbv_task_debug_console_task(void *param)
-// {
-//     sbv_rtos_tick_type_t tick_to_wait = sbv_rtos_ms_to_tick(100);
-
-//     for(;;)
-//     {
-//         sbv_debug_tx_logging();
-
-//         sbv_rtos_task_delay(tick_to_wait);
-//     }
-// }
-
-// int
-// sbv_task_uart_rx_cb (uint8_t *data, const uint16_t size)
-// {
-//     if (! data || ! size)
-//         return -1;
-
-//     sbv_task_print_rx (data, size);
-//     sbv_debug_command_handle((char *)data, size);
-
-//     return 0;
-// }
-
 /*
  * Task for debugging UART rx using interrupt and RTOS direct task notification
  */
@@ -160,78 +126,3 @@ sbv_task_uart_rx_task(void *param)
         sbv_uart_rx_rcv_data (sbv_uart_1, &rcv_data_size, timeout_ms);
     }
 }
-
-// static void
-// sbv_task_can_tx_data_fill(uint8_t *can_data, uint8_t num)
-// {
-//     uint8_t i;
-
-//     for(i = 0; i < SBV_CAN_DATA_MAX_SIZE; ++i)
-//         *(can_data + i) = num + i;
-// }
-
-// /*
-//  * Task for debugging RTOS implementation of CAN TX
-// */
-// void
-// sbv_task_can_tx_debug_task(void *param)
-// {
-//     uint8_t num = 65;
-//     uint8_t can_tx_data[SBV_CAN_DATA_MAX_SIZE];
-//     sbv_rtos_tick_type_t tick_to_wait = sbv_rtos_ms_to_tick(150);
-
-//     // sbv_rtos_task_delay(sbv_rtos_ms_to_tick(4000));
-
-//     for(;;)
-//     {
-//         if(num > 122)
-//             num = 65;
-//         else
-//             num += 8;
-
-//         sbv_task_can_tx_data_fill(can_tx_data, num);
-
-//         sbv_can_send_data(SBV_CAN_MSG_LOGGING, can_tx_data, SBV_CAN_DATA_MAX_SIZE);
-
-//         sbv_rtos_task_delay(tick_to_wait);
-//     }
-// }
-// /*
-//  * Task for debugging RTOS implementation of CAN RX
-// */
-// void sbv_task_can_rx_debug_task(void *param)
-// {
-//     uint8_t* can_rx_data = NULL;
-//     uint8_t  can_rx_len = 0;
-//     uint16_t pkt_id;
-
-//     for(;;)
-//     {
-//         can_rx_data = sbv_can_rcv_data(&can_rx_len, &pkt_id);
-//         if(can_rx_data && can_rx_len)
-//             sbv_task_print_rx(can_rx_data, can_rx_len);
-
-//         can_rx_len  = 0;
-//         can_rx_data = NULL;
-//     }
-// }
-
-// /*
-//  * Task for handling packet received via CAN
-// */
-// void sbv_task_can_rx_handle(void *param)
-// {
-//     uint8_t* can_rx_data = NULL;
-//     uint8_t can_pkt_len = 0;
-//     uint16_t can_pkt_id;
-
-//     for(;;)
-//     {
-//         can_rx_data = sbv_can_rcv_data(&can_pkt_len, &can_pkt_id);
-//         if(can_rx_data && can_pkt_len)
-//         {
-//             sbv_can_pkt_process(can_rx_data, can_pkt_len, can_pkt_id);
-//             sbv_task_print_rx(can_rx_data, can_pkt_len);
-//         }
-//     }
-// }
